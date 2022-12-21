3da5134a961d84bf31f25de7d9abb62f2e33018c
Ryan Sanderson <ryanbsanderson@gmail.com>
2022-12-20 20:36:41 -0600

    General development.

--- README.md
+++ README.md
@@ -1,22 +1,15 @@
-# u l 7 1 m 4 7 3   n u m b 3 r   6 u 3 5 5
+# u l 7 1 m 4 7 3 &nbsp; n u m b 3 r &nbsp; 6 u 3 5 5
 
 ## Introduction
-
-ipsum
+This is one of my very first solo Python projects. The ol' number guessing game is a staple of beginner Python projects. This coding project was created as a fun way to experiment with a variety of new (to me) programming techniques.
 
 ## Features:
-* ipsum.
-* ipsum.
-
+* Various difficulty levels.
+* Reading and writing from/to a CSV leaderboard.
+* Username input that only accepts initials using regex sorting.
+* Typewriter text effect.
 
 ## Main technologies:
 * Python 3.9.2
-
-
-## How it works:
-
-
-
-
 
 <!--- ctrl-shift-v to open preview --->
--- ult_num_guess.py
+++ ult_num_guess.py
@@ -1,30 +1,19 @@
+# Built-in libraries
+import csv
+import curses
 import os
 import random
-import re  # regex
+import re
 import time
 
-
-def main_menu(name):
-    """
-    This module is the main menu.
-
-    Args:
-        name (str): User's initials.
-    Returns:
-        XXXXXXXX fill in later XXXXXXXXXXXXXXXX.
-    """
-
-    print(typewriter("...", 1.0))
-
-    welcome_msg = f"> User {name} successfully logged in.\n"
-
-    print(typewriter(welcome_msg, 0.05))
+# 3rd-party libraries
+import getch
 
 
 def check_name():
     """
-    This module accepts the user's name and checks it for invalid characters
-    using a regex pattern.
+    Accept the user's name and checks it for invalid characters using a regex
+    pattern.
 
     Returns:
         Returns the user's name once it has been verified.
@@ -33,26 +22,67 @@ def check_name():
 
     while True:
 
-        name_input = input(typewriter("> Enter your initials: ", 0.05)).upper()
+        name_input = input(typewriter("> Enter your initials: ")).upper()
 
         if re.match(initials_pattern, name_input):
+            print(typewriter(f"> User {name_input} successfully logged in."))
             return name_input
         else:
             print("Invalid initials.")
 
 
-def random_number(name):
+def main_menu(name):
     """
-    This module sets the difficulty and runs the number guessing conditional
-    statements.
+    Displays options for the user to choose.
+
+    Args:
+        name (str): User's initials.
+    """
+
+    print("\nMAIN MENU:")
+    print(typewriter(f"  > Choose an option. "))
+
+    print("\n  [1] Play game")
+    print("  [2] Leaderboard")
+    print("  [3] Quit\n")
+
+    # Accept input without the user pressing <enter>.
+    while True:
+        key = getch.getch()
+        if key == "1":
+            game_code(name)
+        if key == "2":
+            get_leaderboard()
+        if key == "3":
+            exit()
+        else:
+            continue
+
+
+def game_code(name):
+    """
+    Set the difficulty and run the number guessing conditional statements.
 
     Args:
         name (str): User's initials.
     Returns:
-        XXXXXXXX fill in later XXXXXXXXXXXXXXXX.
+        guess_count (int): The number of guesses the user took to win.
     """
-    random_num = random.randint(1, 100)
+    difficulty = int(
+        input("> Set your difficulty level. (1 - Easy, 2 - Med, 3 - Hard)\n")
+    )
+
+    if difficulty == 1:
+        high_limit = 10
+    elif difficulty == 2:
+        high_limit = 100
+    elif difficulty == 3:
+        high_limit = 1000
+
+    random_num = random.randint(1, high_limit)
     guess_count = 1
+
+    print(typewriter(f"> Guess a number between 1 and {high_limit}\n"))
 
     while guess_count <= 10:
         guess = int(input(f"Guess {guess_count}: "))
@@ -67,17 +97,73 @@ def random_number(name):
             print(
                 typewriter(
                     f"> Well done, {name}! You guessed the number "
-                    f"{guess} in {guess_count} tries.",
-                    0.05,
+                    f"{guess} in {guess_count} tries.\n"
                 )
             )
-            break
-    return
+            # Add score to CSV file for storage.
+            update_leaderboard(name, guess_count)
+            main_menu(name)
+
+        else:
+            print("Invalid input.")
+
+    print("YOU LOSE! ")
+
+    return guess_count
 
 
-def typewriter(text, delay):
+def update_leaderboard(name, guess_count):
     """
-    This module creates a typewrite effect for print statements.
+    Add the user's score to a leaderboard CSV file.
+
+    Args:
+        name (str): User's initials.
+        guess_count (int): The number of guesses the use took to win.
+    """
+    # Open CSV file in write mode.
+    with open("leaderboard.csv", "a", newline="") as leaderboard:
+        # Create the CSV writer.
+        writer = csv.writer(leaderboard)
+        # Write a row to the CSV file.
+        writer.writerow([name, guess_count])
+
+
+def get_leaderboard():
+    """
+    Retrieve the leaderboard data and sort it
+
+    Args:
+        name (str): User's initials.
+        guess_count (int): The number of guesses the use took to win.
+    """
+    # Open the leaderboard in read-only mode.
+    with open("leaderboard.csv", "r") as leaderboard:
+        # Create a CSV reader.
+        reader = csv.reader(leaderboard)
+        # Sort rows by score in ascending order.
+        sorted_rows = sorted(
+            reader,
+            key=lambda row: int(row[1]) if row[1].isdigit() else 0,
+            reverse=False,
+        )
+
+        print("THE 10 BEST PLAYERS\n")
+        print("INITIAL     SCORE")
+        for i, row in enumerate(sorted_rows):
+            print(row[0], "       ", row[1])
+
+            if i == 9:
+                break
+
+        print("\nPress <enter> to return")
+        input()
+
+    main_menu(name)
+
+
+def typewriter(text, delay=0.04):
+    """
+    Create a typewrite effect for print statements.
 
     Args:
         text (str): Text to be printed.
@@ -98,13 +184,14 @@ os.system("clear")
 
 print("u l 7 1 m 4 7 3   n u m b 3 r   6 u 3 5 5\n")
 
+# Get the user's initials.
 name = check_name()
 
+# Run main menu.
 main_menu(name)
 
-print(typewriter("> Guess a number between 1 and 100\n", 0.05))
-
-random_number(name)
+# Run logic for the game.
+game_code(name)
 
 
 print("\nG A M E   O V E R")
